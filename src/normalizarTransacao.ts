45bf8b6f0b303eeca6fbc3731292d1f5d3a38c37
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-06-05 10:20:50 +0200

    funcoes de normalizar transacao (interface) e conversao de valores string para number

---
+++ src/normalizarTransacao.ts
@@ -0,0 +1,47 @@
+import moedaParaNumero from "./moedaParaNumero.js";
+
+declare global {
+  type TransacaoPagamentos = "Boleto" | "Cartão de Crédito";
+  type TransacaoStatus =
+    | "Paga"
+    | "Recusada pela operadora de cartão"
+    | " Aguardando pagamento"
+    | "Estornada";
+
+  interface TransacaoAPI {
+    Data: string;
+    Email: string;
+    ID: number;
+    Nome: string;
+    Status: TransacaoStatus;
+    ["Valor (R$)"]: string;
+    ["Cliente Novo"]: number;
+    ["Forma de Pagamento"]: TransacaoPagamentos;
+  }
+
+  interface Transacao {
+    data: string;
+    email: string;
+    id: number;
+    nome: string;
+    status: TransacaoStatus;
+    moeda: string;
+    valor: number | null;
+    pagamento: TransacaoPagamentos;
+    novo: boolean;
+  }
+}
+
+export default function normalizarTransacao(transacao: TransacaoAPI) {
+  return {
+    data: transacao.Data,
+    email: transacao.Email,
+    id: transacao.ID,
+    nome: transacao.Nome,
+    status: transacao.Status,
+    moeda: moedaParaNumero(transacao["Valor (R$)"]),
+    valor: 0,
+    pagamento: transacao["Forma de Pagamento"],
+    novo: Boolean(transacao["Cliente Novo"]),
+  };
+}
--- src/script.ts
+++ src/script.ts
@@ -1,34 +1,12 @@
 import fetchData from "./fetchData.js";
-
-type TransacaoPagamentos = "Boleto" | "Cartão de Crédito";
-type TransacaoStatus =
-  | "Paga"
-  | "Recusada pela operadora de cartão"
-  | " Aguardando pagamento"
-  | "Estornada";
-
-interface TransacaoAPI {
-  Data: string;
-  Email: string;
-  ID: number;
-  Nome: string;
-  Status: string;
-  ["Valor (R$)"]: string;
-  ["Cliente Novo"]: number;
-  ["Forma de Pagamento"]: TransacaoPagamentos;
-}
+import normalizarTransacao from "./normalizarTransacao.js";
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
-    "https://api.origamid.dev/json/transacoes.json"
+    "https://api.origamid.dev/json/transacoes.json?"
   );
-
-  console.log(data);
-
-  if (data) {
-    data.forEach((item) => {
-      console.log(item.Email);
-    });
-  }
+  if (!data) return;
+  const transacoes = data.map(normalizarTransacao);
+  console.log(transacoes);
 }
 handleData();
